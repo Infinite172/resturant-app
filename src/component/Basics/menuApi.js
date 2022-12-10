b00c9405638b3368931eed1f6ac821d0f959a1da
Karkipranish172 <108966065+Infinite172@users.noreply.github.com>
2022-12-10 17:57:02 +0545

    Add files via upload

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,44 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/1.jpg",
+    name: "maggi",
+    category: "Lunch",
+    price: "$25",
+    description: "aaloremasdfsa",
+  },
+  {
+    id: 2,
+    image: "images/2.jpg",
+    name: "rice",
+    category: "Supper",
+    price: "$251",
+    description: "bbloremasdfsa",
+  },
+  {
+    id: 3,
+    image: "images/3.jpg",
+    name: "Khir",
+    category: "Dinner",
+    price: "$253",
+    description: "ccloremasdfsa",
+  },
+  {
+    id: 4,
+    image: "images/4.jpg",
+    name: "Pasta",
+    category: "Breakfast",
+    price: "$254",
+    description: "ddloremasdfsa",
+  },
+  {
+    id: 5,
+    image: "images/5.jpg",
+    name: "Noodles",
+    category: "Karki",
+    price: "$254",
+    description: "eeloremasdfsa",
+  },
+];
+
+export default Menu;
